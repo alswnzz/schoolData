cef9023522ce0b9b03aecde93b7e0f4e43a2d837
user <user@DESKTOP-CHPCDP8>
2019-10-15 10:08:22 +0900

    create repo

---
+++ schoolData/src/day04/start01.java
@@ -0,0 +1,13 @@
+package day04;
+
+public class start01 {
+	
+	public start01() {
+		System.out.println("zzzz");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new start01();
+	}
+
+}
